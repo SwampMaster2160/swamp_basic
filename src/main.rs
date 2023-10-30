598bf57a7dddc033132fd0b8872f77feaf2e4e39
SwampMaster2160 <jaxon1454@gmail.com>
2023-10-30 14:17:28 +1300

    Setup IO Loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::stdin;
+
 fn main() {
-	println!("Hello, world!");
+	println!("--- Swamp BASIC, type \"exit\" to exit interpreter. ---");
+	'main_loop: loop {
+		let mut input_text = String::new();
+		stdin().read_line(&mut input_text).unwrap();
+		for line in input_text.lines() {
+			if line == "exit" {
+				break 'main_loop;
+			}
+			interpret_line(line);
+		}
+	}
+}
+
+fn interpret_line(line: &str) {
+	println!("{}", line)
 }
