44c1aa39978148ad9952bf20ae14e5298d404732
SwampMaster2160 <jaxon1454@gmail.com>
2023-11-10 12:23:29 +1300

    Added ScalarValue::as_index()

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,14 @@
 use std::{error::Error, fmt::Display};
 
+use crate::{scalar_value::ScalarValue, lexer::type_restriction::TypeRestriction};
+
 #[derive(Debug, Clone)]
 pub enum BasicError {
 	CharEscapeInvalidChar(char),
 	CharEscapeAtLineEnd,
-	InvalidNonAlphabeticOperator(String)
+	InvalidNonAlphabeticOperator(String),
+	IndexOutOfBounds(ScalarValue, usize),
+	TypeMismatch(ScalarValue, TypeRestriction),
 }
 
 impl Display for BasicError {
@@ -13,6 +17,8 @@ impl Display for BasicError {
 			Self::CharEscapeInvalidChar(chr) => write!(formatter, "Invalid character '{chr}' after '\\' escape character."),
 			Self::CharEscapeAtLineEnd => write!(formatter, "'\\' string escape character at end of line."),
 			Self::InvalidNonAlphabeticOperator(operator) => write!(formatter, "\"{operator}\" is an invalid non-alphabetic operator."),
+			Self::IndexOutOfBounds(..) => todo!(),
+			Self::TypeMismatch(..) => todo!(),
 		}
 	}
 }
--- src/scalar_value.rs
+++ src/scalar_value.rs
@@ -1,9 +1,9 @@
 use std::rc::Rc;
 
-use num::{BigInt, complex::Complex64};
+use num::{BigInt, complex::Complex64, bigint::Sign, ToPrimitive};
 use strum_macros::EnumDiscriminants;
 
-use crate::lexer::type_restriction::TypeRestriction;
+use crate::{lexer::type_restriction::TypeRestriction, error::BasicError};
 
 #[derive(Debug, Clone, PartialEq, EnumDiscriminants)]
 pub enum ScalarValue {
@@ -45,6 +45,45 @@ impl ScalarValue {
 			TypeRestriction::Number => self.is_integer() || self.is_float(),
 			TypeRestriction::String => matches!(self, Self::String(_) | Self::Char(_) | ScalarValue::EmptyString),
 			TypeRestriction::Boolean => matches!(self, Self::Bool(_)),
+		}
+	}
+
+	/// Given a length of a container, will return Ok(index) if the value can be used as a index for the container or returns an error otherwise
+	pub fn as_index(&self, container_length: usize) -> Result<usize, BasicError> {
+		match self {
+			Self::Size(index) => {
+				let index = *index;
+				match index < container_length {
+					true => Ok(index),
+					false => Err(BasicError::IndexOutOfBounds(self.clone(), container_length)),
+				}
+			}
+			ScalarValue::Byte(index) => {
+				// A usize is at least 16 bits wide
+				let index = *index as usize;
+				match index < container_length {
+					true => Ok(index),
+					false => Err(BasicError::IndexOutOfBounds(self.clone(), container_length)),
+				}
+			}
+			ScalarValue::BigInteger(index) => {
+				// If the index is negative, add the length of the container to convert it from the -container_length..0 range to the 0..container_length range
+				let index = match index.sign() == Sign::Minus {
+					false => index.clone(),
+					true => Rc::new(index.as_ref().clone() + BigInt::from(container_length))
+				};
+				// The index is invalid if it is not between 0 and usize::MAX
+				let index_as_usize = match index.to_usize() {
+					Some(index) => index,
+					None => return Err(BasicError::IndexOutOfBounds(self.clone(), container_length)),
+				};
+				// The index is invalid if it is not less than the container length
+				match index_as_usize < container_length {
+					true => Ok(index_as_usize),
+					false => Err(BasicError::IndexOutOfBounds(self.clone(), container_length)),
+				}
+			}
+			_ => Err(BasicError::TypeMismatch(self.clone(), TypeRestriction::Integer)),
 		}
 	}
 }
